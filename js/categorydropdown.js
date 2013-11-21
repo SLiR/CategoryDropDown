542360954c7c13e8ccc78f6b83771b04c1ff54c4
SLiR <mn.slir@gmail.com>
2013-11-20 19:46:03 -0800

    JavaScript modifications
    Added code to handle window location hash to retain drop down selections
    on page reload/refresh.

--- js/categorydropdown.js
+++ js/categorydropdown.js
@@ -2,11 +2,12 @@
 
     $.fn.CategoryDropDown = function(options){
 
+        this.dropdowns = [],
+
         options = $.extend({}, $.fn.CategoryDropDown.defaultOptions, options);
 
         // dropdown handles the UL elements located inside the first level LI elements.
         var dropdown = function(el, innerCDD){
-            el.dropdown         = 'test';
             this.cdd            = innerCDD;
             this.el             = el;
             this.placeholder    = this.el.find('> span');
@@ -21,6 +22,8 @@
         dropdown.prototype = {
             initEvents : function(){
                 var obj = this;
+                // Store this dropdown as a data object with jQuery
+                $.data(obj.el[0], 'CategoryDropDown', obj);
 
                 obj.el.on('click', function(){
                     $(this).toggleClass(options.activeClass);
@@ -31,10 +34,10 @@
                     return obj.doSelect($(this));
                 });
             },
-            doSelect : function(opt){
+            doSelect : function(opt, setHash){
                 if(opt){
                     var attr = opt.attr('value');
-                    this.selectedValue = (attr != null ? attr : $.trim(opt.text()) );
+                    this.selectedValue = $.trim( (typeof attr !== 'undefined' ? attr : opt.text()) );
                     this.selectedIndex = opt.index();
                     this.placeholder.text(this.title + ': ' + this.selectedValue);
                 } else {
@@ -44,38 +47,62 @@
                 }
                 // Make sure the dropdown can collapse
                 this.el.removeClass(options.activeClass);
-                this.cdd.select(this);
+                // Call select on the parent, which is our CategoryDropDown container.
+                this.cdd.Select(this, setHash);
                 return false;
             }
         };
 
-        this.select = function(dropdown){
+        this.HashValue = function(value){
+            var value = (typeof value !== 'undefined') ? value : this.selectedValue;
+            return (value == '') ? '' : '?' + options.idkey + '=' + value;
+        };
+
+        this.SelectedValue = function(value){
+            if(typeof value !== 'undefined'){
+                var winLocation = ('' + window.location).split('#');
+                var winHash     = winLocation[1];
+                var curHash     = this.HashValue();
+                var newHash     = this.HashValue(value);
+                var newWinLocation = '';
+
+                if(typeof winHash === 'undefined') winHash = '';
+
+                if(winHash != '' && curHash != '' && winHash.indexOf(curHash) !== -1)
+                    newWinLocation = winHash.replace(curHash, newHash);
+                else
+                    newWinLocation = winHash + newHash;
+
+                location.replace(winLocation[0] + '#' + newWinLocation);
+                this.selectedValue = value;
+            } else {
+                return this.selectedValue;
+            }
+        };
+
+        this.Select = function(dropdown, setHash){
             if(!dropdown) return;
+            // Make sure setHash is set to true by default.
+            var setHash = (typeof setHash !== 'boolean') ? true : setHash;
+
             if(this.lastDropdown && this.lastDropdown !== dropdown)
                 this.lastDropdown.doSelect();
 
             if(dropdown.selectedIndex > -1)
             {
-                // var newIndex = [$(this).index(), dropdown.el.index(), dropdown.selectedIndex];
-
-                // // http://stackoverflow.com/questions/1773069/using-jquery-to-compare-two-arrays
-                // // Use jQuery to compare two arrays.
-                // if(this.selectedIndex &&
-                //     $(this.selectedIndex).not(newIndex).length == 0 &&
-                //     $(newIndex).not(this.selectedIndex).length == 0 ) return;
-
                 if(this.selectedValue && this.selectedValue === dropdown.selectedValue)
                     return;
 
                 this.lastDropdown = dropdown;
-                //this.selectedIndex = newIndex;
-                this.selectedValue = dropdown.selectedValue;
-                //window.location.hash = 'cdd[' + this.selectedIndex + ']';
-                window.location.hash = 'cdd[' + this.selectedValue + ']';
+                if(setHash)
+                    this.SelectedValue(dropdown.selectedValue);
+                else
+                    this.selectedValue = dropdown.selectedValue;
             }
             else
             {
                 this.selectedIndex = null;
+                this.SelectedValue('');
                 this.lastDropdown = null;
             }
         };
@@ -88,40 +115,49 @@
                 this.lastDropdown   = null;
                 this.selectedVal    = null;
                 this.selectedIndex  = null;
-                this.select         = cdd.select;
+                this.selectedValue  = '';
+                this.Select         = cdd.Select;
+                this.HashValue      = cdd.HashValue;
+                this.SelectedValue  = cdd.SelectedValue;
 
                 $(this).find('> li').each(function(){
-                    new dropdown($(this), innerCDD);
+                    var dd = new dropdown($(this), innerCDD);
+                    cdd.dropdowns.push(dd);
                 });
             });
 
-            //if(window.location.hash)
-            //alert(window.location.hash);
-
-            // var regex = /(?!cdd\[)(\d+(?!=,*))(?!=\])/gi;
-            var regex = /(?!cdd\[)(\w)+(?=\])/gi;
-            if(regex.test(window.location.hash))
-            {
-                var fromHash = window.location.hash.match(regex);
-
-                var selItem = $(this).find('li ul li:contains("' + fromHash + '"):first');
-                if(selItem.length > 0)
-                {
-                    alert(selItem.html());
-                    selItem.click();
-                    // var parent = selItem.closest('li:first');
-                    // alert(parent.length);
-                    // if(parent.length > 0){
-                    //     alert(parent.dropdown);
-                    //     parent.click();
-                    // }
-                }
-            }
+            cdd.TrackHash();
 
             $(document).click(function(){
                 cdd.find('> li').removeClass(options.activeClass);
             })
+        };
 
+        this.TrackHash = function(){
+            // Make sure setHash is set to true by default.
+            var regex = new RegExp(options.idkey + '=([^?]*)', 'gi');
+            var winHash = window.location.hash;
+            var match = regex.exec(winHash);
+
+            while(match != null){
+                var value = match[1];
+                var selector = 'li ul li:contains("' + value + '"):first,' +
+                               'li ul li[value="' + value + '"]';
+                var selItem = $(this).find(selector);
+
+                if(selItem.length > 0)
+                {
+                    var domParent = selItem.parents('li');
+                    var domDropDown = (domParent.length > 0) ?
+                                        $.data(domParent[0], 'CategoryDropDown') :
+                                        null;
+
+                    if(domDropDown != null)
+                        domDropDown.doSelect(selItem, false);
+                }
+
+                match = regex.exec(winHash);
+            }
         };
 
         this.init();
@@ -130,7 +166,7 @@
     };
 
     $.fn.CategoryDropDown.defaultOptions = {
-              class:    'categorydropdown',
+              idkey:    'catdd',
         activeClass:    'active'
     };
 
